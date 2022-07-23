fe4d3e81ded8848f433d220467d94dd75674fb1a
guzzye13 <eddyguzman322@gmail.com>
2022-07-23 10:44:41 -0400

    Initial commit

---
+++ magicians.py
@@ -0,0 +1,9 @@
+magicians = ['alice', 'david','carolina']
+for magician in magicians: # Defined a for loop
+#    prints each magician in magicians
+   # print(magician)
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+# Doing something after a for loop
+print("Thank you, everyone. That was a great magic show!")
